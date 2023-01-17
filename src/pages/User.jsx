9d66527d05d3934948d12a10c6450f983729d36b
Idris110 <ratlamwalaidris110@gmail.com>
2023-01-17 08:54:38 +0530

    initiate user page

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,4 +1,5 @@
 import { FaCodepen, FaStore, FaUserFriends, FaUsers } from 'react-icons/fa'
+import LoadingAnim from '../components/layout/assets/loading.gif'
 import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
@@ -6,37 +7,48 @@ import { Link } from 'react-router-dom'
 function User() {
 
     const [user, setUser] = useState({})
+    const [loading, setLoading] = useState(true);
     const params = useParams()
 
     useEffect(() => {
+        const lo = () => {
+            setLoading(false);
+        }
         fetchUser()
-        // setLoading(false)
+        // setLoading(false);
+        if (document.readyState === 'complete') {
+            setLoading(false)
+        }
+        else {
+            window.addEventListener('load', lo);
+            // Remove the event listener when component unmounts
+            return () => window.removeEventListener('load', lo);
+        }
     }, [])
-    
+
     const cap = (string) => (string.charAt(0).toUpperCase() + string.slice(1));
     const fetchUser = async () => {
         console.log("fetching data");
         // try {
-            console.log("login:"+params.login);
-            const response = await fetch(`https://api.github.com/users/${params.login}`, {
-                headers: {
-                    Authorization: `token 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`
-                    // Authorization: `token ${process.env.KEY}`
-                }
-            })
-            console.log("Res status:"+ response.status);            
-            if (response.status === 404) {
-                window.location = '/notfound'
+        console.log("login:" + params.login);
+        const response = await fetch(`https://api.github.com/users/${params.login}`, {
+            headers: {
+                Authorization: `token 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`
+                // Authorization: `token ${process.env.KEY}`
             }
-            else {
-                // const data = await response.json();
-                // console.log(data);
-                // setUser(data);
-                response.json().then((data) => {
-                    console.log(data);
-                    setUser(data);
-                });
-            }
+        })
+        if (response.status === 404) {
+            window.location = '/notfound'
+        }
+        else {
+            const data = await response.json();
+            console.log(data);
+            setUser(data);
+            // response.json().then((data) => {
+            //     console.log(data);
+            //     setUser(data);
+            // });
+        }
         //   } catch (error) {
         //     console.log('There was an error', error);
         //   }
@@ -58,8 +70,13 @@ function User() {
         public_gists,
         hireable } = user;
 
+    if (loading) return (
+        <div className="flex-1 flex justify-center items-center font-bold mb-20">
+            <img src={LoadingAnim} width="80px" alt="Loading..." />
+        </div>
+    )
     return (<>
-        <div className="mx-auto lg:w-10/12" style={{width : "90%"}}>
+        <div className="mx-auto lg:w-10/12" style={{ width: "90%" }}>
             <div className="mb-4">
                 <Link to="/" className='btn btn-ghost'>Back to Search</Link>
             </div>
@@ -75,14 +92,42 @@ function User() {
                         </div>
                     </div>
                 </div>
-                <div className="col-span-2">
-                    <div className="mb-6">
-                        <h1 className="text-3xl card-title">{name}
-                            <div className="ml-2 mr-1 badge-success badge">{type}
+                <div className="col-span-2 mt-1">
+                    <div className="mb-5">
+                        <h1 className="text-2xl card-title">{name}
+                            <div className="ml-2 mr-1 mt-1 badge-success badge">{type}
                             </div>
-                            {!hireable ? <div className="mx-1 badge badge-info">Hireable</div>
-                                :""}
+                            {!hireable ? <div className="mx-1 mt-1 badge badge-info">Hireable</div>
+                                : ""}
                         </h1>
+                        <p className='mt-4'>{bio}</p>
+                        <a href={html_url} target="_blank" rel='noreferrer' className='btn btn-outline mt-5'>
+                            Visit github profile
+                        </a>
+                    </div>
+                    <div className="w-full shadow-md rounded-lg stats bg-base-100">
+                        {location && (
+                            <div className="stat">
+                                <div className="stat-title text-md">
+                                    Location
+                                    <div className="stat-value text-lg">
+                                        {location}
+                                    </div>
+                                </div>
+                            </div>
+                        )}
+                        {twitter_username && (
+                            <div className="stat">
+                                <div className="stat-title text-md">
+                                    Twitter
+                                    <div className="stat-value text-lg">
+                                        <a href={`https://twitter.com/${twitter_username}`} target='_blank' rel='noreferrer' >
+                                            {twitter_username}
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        )}
                     </div>
                 </div>
             </div>
